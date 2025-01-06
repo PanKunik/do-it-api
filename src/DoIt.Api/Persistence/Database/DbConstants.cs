d52ec6547557d5b4a521baaa14105de47378ab27
PanKunik <patryk.kunicki@pm.me>
2025-01-06 20:03:37 +0100

    Feature #1 - Integrating with PostgreSQL using Dapper (part II).

---
+++ src/DoIt.Api/Persistence/Database/DbConstants.cs
@@ -0,0 +1,7 @@
+﻿namespace DoIt.Api.Persistence.Database
+{
+    public static class DbConstants
+    {
+        public static readonly string DoItDbConnectionStringPath = "Database:ConnectionStrings:DoItDb";
+    }
+}
---
+++ src/DoIt.Api/Persistence/Database/IDbConnectionFactory.cs
@@ -0,0 +1,9 @@
+﻿using System.Data;
+
+namespace DoIt.Api.Persistence.Database
+{
+    public interface IDbConnectionFactory
+    {
+        Task<IDbConnection> CreateConnectionAsync();
+    }
+}
