f7af8860bf7ff9e3f6464ced6c429d9dd05266a6
PanKunik <patryk.kunicki@pm.me>
2025-01-08 20:12:54 +0100

    Feature #15 - Allow to get list of all tasks

---
+++ src/DoIt.Api/Controllers/Tasks/GetTaskResponse.cs
@@ -0,0 +1,9 @@
+﻿namespace DoIt.Api.Controllers.Tasks;
+
+public sealed record GetTaskResponse(
+    Guid taskId,
+    string title,
+    DateTime createdAt,
+    bool isDone,
+    bool isImportant
+);
---
+++ tests/DoIt.Api.Unit.Tests/Controllers/Tasks/TasksControllerTests.cs
@@ -0,0 +1,64 @@
+﻿using DoIt.Api.Controllers.Tasks;
+using DoIt.Api.Persistence.Repositories.Tasks;
+using FluentAssertions;
+using Microsoft.AspNetCore.Mvc;
+using NSubstitute;
+using System.Net;
+
+namespace DoIt.Api.Unit.Tests.Controllers.Tasks;
+
+public class TasksControllerTests
+{
+    private readonly TasksController _cut;
+    public TasksControllerTests()
+    {
+        var tasksRepository = Substitute.For<ITasksRepository>();
+        tasksRepository.GetAll().Returns(new List<TaskDTO>());
+
+        _cut = new TasksController(tasksRepository);
+    }
+
+    [Fact]
+    public async Task Get_WhenInvoked_ShouldReturnOkObjectResult()
+    {
+        // Act
+        var result = await _cut.Get();
+
+        // Assert
+        result
+            .Should()
+            .NotBeNull();
+
+        result
+            .Should()
+            .BeOfType<OkObjectResult>();
+    }
+
+    [Fact]
+    public async Task Get_WhenInvoked_ShouldReturn200OKStatusCode()
+    {
+        // Act
+        var result = (OkObjectResult)(await _cut.Get());
+
+        // Assert
+        result.StatusCode
+            .Should()
+            .Be((int)HttpStatusCode.OK);
+    }
+
+    [Fact]
+    public async Task Get_OnSuccess_ShouldReturnListOfTasks()
+    {
+        // Act
+        var result = (OkObjectResult)(await _cut.Get());
+
+        // Assert
+        result.Value
+            .Should()
+            .NotBeNull();
+
+        result.Value
+            .Should()
+            .BeOfType<List<GetTaskResponse>>();
+    }
+}
