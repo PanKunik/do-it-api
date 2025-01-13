312b64aa135ec5085a9dd6cb1451e7092f4bffa5
PanKunik <patryk.kunicki@pm.me>
2025-01-13 19:34:04 +0100

    Refactor #11 - Add missing tests after integrating with database

---
+++ tests/DoIt.Api.Integration.Tests/Tasks/CreateTasksControllerTests.cs
@@ -0,0 +1,44 @@
+﻿using DoIt.Api.Controllers.Tasks;
+using System.Net.Http.Json;
+
+namespace DoIt.Api.Integration.Tests.Tasks;
+
+[Collection("Tasks controller tests")]
+public class CreateTasksControllerTests
+    : IAsyncLifetime
+{
+    private readonly HttpClient _client;
+    private readonly Func<Task> _resetDatabase;
+
+    public CreateTasksControllerTests(DoItApiFactory apiFactory)
+    {
+        _client = apiFactory.HttpClient;
+        _resetDatabase = apiFactory.ResetDatabaseAsync;
+    }
+
+    [Fact]
+    public async Task Create_WhenInvokedWithProperData_ShouldSaveInDatabase()
+    {
+        // Act
+        await _client.PostAsJsonAsync(
+            "api/tasks",
+            new CreateTaskRequest("Test title 1")
+        );
+
+        // Assert
+        var tasksInDatabase = await _client.GetFromJsonAsync<List<GetTaskResponse>>("api/tasks");
+
+        tasksInDatabase
+            .Should()
+            .HaveCount(1);
+
+        tasksInDatabase!
+            .First().Title
+            .Should()
+            .BeEquivalentTo("Test title 1");
+    }
+
+    public async Task InitializeAsync() => await Task.CompletedTask;
+
+    public Task DisposeAsync() => _resetDatabase();
+}
